e6032d3c2c24a917f9d6599531e2c0e150a9e828
chesnokov-mx <kkalibri@icloud.com>
2022-04-04 09:58:26 +0300

    4april

--- server.js
+++ server.js
@@ -69,6 +69,8 @@ const express = require("express");
 const app = express();
 const fs = require("fs");
 
+
+
 app.get("/", function (req, res) {
     res.sendFile(__dirname + "/html/index.html");
 });
@@ -85,27 +87,94 @@ app.get("/Contacts", function (req, res) {
 
 app.use(express.static(__dirname + '/public'));
 
-app.get("/video", function (req, res) {
+
+
+const filePath = "./public/VIDEO/video.mp4";
+
+app.get('/works-in-chrome-and-safari', (req, res) => {
+
+    const options = {};
+
+    let start;
+    let end;
+
     const range = req.headers.range;
-    if (!range) {
-        res.status(400).send("Requires Range header");
+    if (range) {
+        const bytesPrefix = "bytes=";
+        if (range.startsWith(bytesPrefix)) {
+            const bytesRange = range.substring(bytesPrefix.length);
+            const parts = bytesRange.split("-");
+            if (parts.length === 2) {
+                const rangeStart = parts[0] && parts[0].trim();
+                if (rangeStart && rangeStart.length > 0) {
+                    options.start = start = parseInt(rangeStart);
+                }
+                const rangeEnd = parts[1] && parts[1].trim();
+                if (rangeEnd && rangeEnd.length > 0) {
+                    options.end = end = parseInt(rangeEnd);
+                }
+            }
+        }
     }
-    const videoPath = "VIDEO/video.mp4";
-    const videoSize = fs.statSync("VIDEO/video.mp4").size;
-    const CHUNK_SIZE = 10 ** 6;
-    const start = Number(range.replace(/\D/g, ""));
-    const end = Math.min(start + CHUNK_SIZE, videoSize - 1);
-    const contentLength = end - start + 1;
-    const headers = {
-        "Content-Range": `bytes ${start}-${end}/${videoSize}`,
-        "Accept-Ranges": "bytes",
-        "Content-Length": contentLength,
-        "Content-Type": "video/mp4",
-    };
-    res.writeHead(206, headers);
-    const videoStream = fs.createReadStream(videoPath, { start, end });
-    videoStream.pipe(res);
+
+    res.setHeader("content-type", "video/mp4");
+
+    fs.stat(filePath, (err, stat) => {
+        if (err) {
+            console.error(`File stat error for ${filePath}.`);
+            console.error(err);
+            res.sendStatus(500);
+            return;
+        }
+
+        let contentLength = stat.size;
+
+        if (req.method === "HEAD") {
+            res.statusCode = 200;
+            res.setHeader("accept-ranges", "bytes");
+            res.setHeader("content-length", contentLength);
+            res.end();
+        }
+        else {
+            let retrievedLength;
+            if (start !== undefined && end !== undefined) {
+                retrievedLength = (end+1) - start;
+            }
+            else if (start !== undefined) {
+                retrievedLength = contentLength - start;
+            }
+            else if (end !== undefined) {
+                retrievedLength = (end+1);
+            }
+            else {
+                retrievedLength = contentLength;
+            }
+
+            res.statusCode = start !== undefined || end !== undefined ? 206 : 200;
+
+            res.setHeader("content-length", retrievedLength);
+
+            if (range !== undefined) {
+                res.setHeader("content-range", `bytes ${start || 0}-${end || (contentLength-1)}/${contentLength}`);
+                res.setHeader("accept-ranges", "bytes");
+            }
+
+            const fileStream = fs.createReadStream(filePath, options);
+            fileStream.on("error", error => {
+                console.log(`Error reading file ${filePath}.`);
+                console.log(error);
+                res.sendStatus(500);
+            });
+
+            fileStream.pipe(res);
+        }
+    });
 });
+
+
+
+
+
 
 app.get("/sidemenu.js", function (req, res) {
     res.sendFile(__dirname + "/js/sidemenu.js");
@@ -120,7 +189,24 @@ app.get("/galleryLoadIN.js", function (req, res) {
     res.sendFile(__dirname + "/js/galleryLoadIN.js");
 });
 
-// app.set('port', process.env.PORT || 8080);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 app.listen(process.env.PORT || 3000, function () {
     console.log("Listening on port 3000!");
